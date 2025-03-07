cc08656f1975beaa2f741103bfe3d8e20d5c55e3
whatsweproject <whatsweproject@gmail.com>
2025-03-07 14:37:55 +0530

    sample_code.py

---
+++ saple_code.py
@@ -0,0 +1,33 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+model_name = "Qwen/QwQ-32B"
+
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    torch_dtype="auto",
+    device_map="auto"
+)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+prompt = "How many r's are in the word \"strawberry\""
+messages = [
+    {"role": "user", "content": prompt}
+]
+text = tokenizer.apply_chat_template(
+    messages,
+    tokenize=False,
+    add_generation_prompt=True
+)
+
+model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
+
+generated_ids = model.generate(
+    **model_inputs,
+    max_new_tokens=32768
+)
+generated_ids = [
+    output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
+]
+
+response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
+print(response)
